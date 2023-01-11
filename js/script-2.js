b936ce8752aa10bdf255ab905b720f096768e376
AntonZinchenko32 <zinchenko309@gmail.com>
2023-01-11 22:22:53 +0200

    финал

--- js/script-2.js
+++ js/script-2.js
@@ -24,42 +24,16 @@ inputIn.addEventListener("keypress", function(event) {
 
 // Функції, змінні
 
-const tweets = [
-    { id: "000", likes: 5, tags: ["js", "nodejs"] },
-    { id: "001", likes: 2, tags: ["html", "css"] },
-    { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
-    { id: "003", likes: 8, tags: ["css", "react"] },
-    { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
-  ];
-  
-  const getTags = tweets =>
-    tweets.reduce((allTags, tweet) => {
-      allTags.push(...tweet.tags);
-  
-      return allTags;
-    }, []);
-  
-  const tags = getTags(tweets);
-  
-  // Вынесем callback-функцию отдельно, а в reducе передадим ссылку на нее.
-  // Это стандартная практика если callback-функция довольно большая.
-  
-  // Если в объекте-аккумуляторе acc нету своего свойства с ключем tag,
-  // то создаем его и записывает ему значение 0.
-  // В противном случае увеличиваем значение на 1.
-  const getTagStats = (acc, tag) => {
-    if (!acc.hasOwnProperty(tag)) {
-      acc[tag] = 0;
-    }
-  
-    acc[tag] += 1;
+const players = [
+  { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+  { name: "Poly", playtime: 469, gamesPlayed: 2 },
+  { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+  { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+];
+// Change code below this line
 
-    return acc;
-  };
-  
-  // Начальное значение аккумулятора это пустой объект {}
-  const countTags = tags => tags.reduce(getTagStats, {});
-  
-  const tagCount = countTags(tags);
+const averagePlaytimePerGame = player.time / player.gamesPlayed;
+
+console.log(totalAveragePlaytimePerGame);
   
   
