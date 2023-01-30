ac9ae2200b9ceee28ec5ff6b906d7288da0de7e0
AntonZinchenko32 <zinchenko309@gmail.com>
2023-01-30 11:35:34 +0200

    2

--- js/script-2.js
+++ js/script-2.js
@@ -1,7 +1,16 @@
-const textInput = document.querySelector(".text-input");
-const output = document.querySelector(".output");
+const openButton = document.querySelector('button[data-action="open"]');
+const closeButton = document.querySelector('button[data-action="close"]');
 
-textInput.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value;
+const instance = basicLightbox.create(`
+	<h1>Dynamic Content</h1>
+	<p>You can set the content of the lightbox with JS.</p>
+`)
+
+openButton.addEventListener("click", () => {
+  instance.show();
+});
+
+closeButton.addEventListener("click", () => {
+  instance.close();
 });
   
